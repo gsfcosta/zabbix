f40a9ae04eb57c200a2711f142767e6810d0a856
Gabriel Silva <42187636+gsfcosta@users.noreply.github.com>
2019-01-18 16:31:25 -0200

    Update zabbix-agent.sh

--- .config/rhel/zabbix-agent.sh
+++ .config/rhel/zabbix-agent.sh
@@ -2,12 +2,12 @@
 vso=$(cat /zabbix/.config/var/versionso.txt)
 vzbx=$(cat /zabbix/.config/var/versionzbx.txt)
 vprimary=$(cat /zabbix/.config/var/versionprimary.txt)
-rpm -Uvh http://repo.zabbix.com/zabbix/$vprimary/rhel/$vso/x86_64/zabbix-agent-$vzbx | dialog --backtitle "LRS Tecnologia LTDA" --infobox "Instalando, aguarde (1/4)..." 0 0
+rpm -Uvh http://repo.zabbix.com/zabbix/$vprimary/rhel/$vso/x86_64/zabbix-agent-$vzbx
 if (( $? != )); then
 	dialog --backtitle "LRS Tecnologia LTDA" --ok-label Sair --msgbox "erro $? - Download do repositório" 0 0	
 	exit
 fi
-yum install zabbix-agent -y | dialog --backtitle "LRS Tecnologia LTDA" --infobox "Instalando, aguarde (2/4)..." 0 0
+yum install zabbix-agent -y
 function zbx(){
 server=$( dialog --stdout --ok-label Confirmar --cancel-label Voltar --title "Configuração do agent" --inputbox 'IP Server:' 0 0 )
 if (( $? == 0 )); then
@@ -35,8 +35,8 @@ fi
 }
 function cont(){
 chmod 777 /run/zabbix -R
-systemctl restart zabbix-agent | dialog --backtitle "LRS Tecnologia LTDA" --infobox "Instalando, aguarde (3/4)..." 0 0
-chkconfig zabbix-agent on | dialog --backtitle "LRS Tecnologia LTDA" --infobox "Instalando, aguarde (4/4)..." 0 0
+systemctl restart zabbix-agent
+chkconfig zabbix-agent on 
 dialog --backtitle "LRS Tecnologia LTDA" --msgbox "Instalação Completa" 0 0
 dialog --backtitle "LRS Tecnologia LTDA" --title "LOG zabbix_agent" --tailbox /var/log/zabbix/zabbix_agentd.log 100 100
 exit
